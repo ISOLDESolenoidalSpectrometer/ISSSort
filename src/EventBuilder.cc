61260731b3de63c9168bf2cb21650b4158ec48ec
Liam Gaffney <lpgaff@gmail.com>
2021-07-02 18:26:48 +0100

    clear the vectors for array events

--- src/EventBuilder.cc
+++ src/EventBuilder.cc
@@ -165,6 +165,17 @@ void EventBuilder::Initialise(){
 
 	hit_ctr = 0;
 	
+	pen_list.clear();
+	nen_list.clear();
+	ptd_list.clear();
+	ntd_list.clear();
+	pid_list.clear();
+	nid_list.clear();
+	pmod_list.clear();
+	nmod_list.clear();
+	prow_list.clear();
+	nrow_list.clear();
+	
 	write_evts->ClearEvt();
 	
 	return;
@@ -415,7 +426,7 @@ unsigned long EventBuilder::BuildEvents( unsigned long start_build ) {
 			
 		} // if close event && hit_ctr > 0
 		
-		if( i % 1000 == 0 || i+1 == n_entries ) {
+		if( i % 10000 == 0 || i+1 == n_entries ) {
 			
 			std::cout << " " << std::setw(8) << std::setprecision(4);
 			std::cout << (float)(i+1)*100.0/(float)n_entries << "%    \r";
