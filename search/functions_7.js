47d0bdb320cc7e3a5536f03cb07174cd8d1c995c
lpgaff <lpgaff@gmail.com>
2025-06-06 19:52:55 +0000

    Deploying to gh-pages from @ ISOLDESolenoidalSpectrometer/ISSSort@5244b104ec9c3522386d3cded34d4a7f06b003ba 🚀

--- search/functions_7.js
+++ search/functions_7.js
@@ -5,5 +5,8 @@ var searchData=
   ['histelabmin_2',['HistElabMin',['../classISSReaction.html#aa20304dfb40bce175b97f3ddc01e209f',1,'ISSReaction']]],
   ['histexbins_3',['HistExBins',['../classISSReaction.html#afd30460140a9c47149db1f4a1942d824',1,'ISSReaction']]],
   ['histexmax_4',['HistExMax',['../classISSReaction.html#a12214c2d82b77e8fd3db9d0fc06b0943',1,'ISSReaction']]],
-  ['histexmin_5',['HistExMin',['../classISSReaction.html#a5b9686e53d4e8e4e1078f1761ef6b671',1,'ISSReaction']]]
+  ['histexmin_5',['HistExMin',['../classISSReaction.html#a5b9686e53d4e8e4e1078f1761ef6b671',1,'ISSReaction']]],
+  ['histgammabins_6',['HistGammaBins',['../classISSReaction.html#a61656e62e77a491bf0fd5dccf28d6099',1,'ISSReaction']]],
+  ['histgammamax_7',['HistGammaMax',['../classISSReaction.html#a02d28a01e3d0f78313fdca7a453294e0',1,'ISSReaction']]],
+  ['histgammamin_8',['HistGammaMin',['../classISSReaction.html#ad30ed7d66a39f688c6c82176ce727c18',1,'ISSReaction']]]
 ];
