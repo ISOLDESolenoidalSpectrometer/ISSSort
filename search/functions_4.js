5459995673a9cd7c455d656d0fef183b4a1467f2
lpgaff <lpgaff@gmail.com>
2025-06-07 00:38:44 +0000

    Deploying to gh-pages from @ ISOLDESolenoidalSpectrometer/ISSSort@1142e0a41e7c5695a0dfac34b87054d4b77eba17 🚀

--- search/functions_4.js
+++ search/functions_4.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['elum_5fenergy_5fcalculator_0',['elum_energy_calculator',['../elum__energy__calculator_8cc.html#a9692c61fd238040fce04f7f734afd12f',1,'elum_energy_calculator.cc']]],
-  ['elumfinder_1',['ElumFinder',['../classISSEventBuilder.html#ab6abc0205e467448e7e5495e1d9001fb',1,'ISSEventBuilder']]]
+  ['ebisonly_0',['EBISOnly',['../classISSConverter.html#aba7326d0b648b9fafd45e178b24b01dc',1,'ISSConverter']]],
+  ['ebiswindow_1',['EBISWindow',['../classISSConverter.html#a9d5367e8f70882f58e2aeaec75f69c7f',1,'ISSConverter']]],
+  ['elum_5fenergy_5fcalculator_2',['elum_energy_calculator',['../elum__energy__calculator_8cc.html#a9692c61fd238040fce04f7f734afd12f',1,'elum_energy_calculator.cc']]],
+  ['elumfinder_3',['ElumFinder',['../classISSEventBuilder.html#ab6abc0205e467448e7e5495e1d9001fb',1,'ISSEventBuilder']]]
 ];
