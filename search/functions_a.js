0dcf336cde7f76491c08e897c660d1b3992bbbf6
lpgaff <lpgaff@gmail.com>
2025-06-29 23:42:57 +0000

    Deploying to gh-pages from @ ISOLDESolenoidalSpectrometer/ISSSort@fb2a38909438f99ab81fc3516e07daa1107d8847 🚀

--- search/functions_a.js
+++ search/functions_a.js
@@ -4,12 +4,13 @@ var searchData=
   ['makehists_1',['MakeHists',['../classISSConverter.html#a1528e5f0ac4e37df224554a30cd34fab',1,'ISSConverter::MakeHists()'],['../classISSEventBuilder.html#a59ef5cacd2f7f6b93a8157b021966acc',1,'ISSEventBuilder::MakeHists()'],['../classISSHistogrammer.html#acd3b9bf14a309c763a966fb12d78a48d',1,'ISSHistogrammer::MakeHists()']]],
   ['makereaction_2',['MakeReaction',['../classISSReaction.html#af09b6b1b50e23050ab80afeff489181f',1,'ISSReaction']]],
   ['maketree_3',['MakeTree',['../classISSConverter.html#a9d3965a923cdd2a95c98c10f095f6ad7',1,'ISSConverter']]],
-  ['mesytecenergy_4',['MesytecEnergy',['../classISSCalibration.html#a22cd07a777e94de09ede135e46427ce1',1,'ISSCalibration']]],
-  ['mesytecthreshold_5',['MesytecThreshold',['../classISSCalibration.html#afc809a4991435ef14f0aaf4e1ba531f6',1,'ISSCalibration']]],
-  ['mesytectime_6',['MesytecTime',['../classISSCalibration.html#ae8bec3d13d576b9566f9323dccbae642',1,'ISSCalibration']]],
-  ['mesytectype_7',['MesytecType',['../classISSCalibration.html#a6327a64635a2cdf0ce0e22ac37c7a6a0',1,'ISSCalibration']]],
-  ['monitor_5frun_8',['monitor_run',['../iss__sort_8cc.html#a41bfdfd4b3dc4ec83a7ad948b00d3993',1,'iss_sort.cc']]],
-  ['multialphagaussianbg_9',['MultiAlphaGaussianBG',['../FitFunctions_8hh.html#a50900a332624e1c01c2adad19a3de747',1,'FitFunctions.hh']]],
-  ['multicrystalballfunctionbg_10',['MultiCrystalBallFunctionBG',['../FitFunctions_8hh.html#a94f13373ad46a7348c5e9d0dc19ef37c',1,'FitFunctions.hh']]],
-  ['mwpcfinder_11',['MwpcFinder',['../classISSEventBuilder.html#a666a793f30d2556ca2a17552d4f75719',1,'ISSEventBuilder']]]
+  ['mapcomparator_4',['MapComparator',['../classISSConverter.html#a86e8ec8ca74a2d60bfa31f7790239368',1,'ISSConverter']]],
+  ['mesytecenergy_5',['MesytecEnergy',['../classISSCalibration.html#a22cd07a777e94de09ede135e46427ce1',1,'ISSCalibration']]],
+  ['mesytecthreshold_6',['MesytecThreshold',['../classISSCalibration.html#afc809a4991435ef14f0aaf4e1ba531f6',1,'ISSCalibration']]],
+  ['mesytectime_7',['MesytecTime',['../classISSCalibration.html#ae8bec3d13d576b9566f9323dccbae642',1,'ISSCalibration']]],
+  ['mesytectype_8',['MesytecType',['../classISSCalibration.html#a6327a64635a2cdf0ce0e22ac37c7a6a0',1,'ISSCalibration']]],
+  ['monitor_5frun_9',['monitor_run',['../iss__sort_8cc.html#a41bfdfd4b3dc4ec83a7ad948b00d3993',1,'iss_sort.cc']]],
+  ['multialphagaussianbg_10',['MultiAlphaGaussianBG',['../FitFunctions_8hh.html#a50900a332624e1c01c2adad19a3de747',1,'FitFunctions.hh']]],
+  ['multicrystalballfunctionbg_11',['MultiCrystalBallFunctionBG',['../FitFunctions_8hh.html#a94f13373ad46a7348c5e9d0dc19ef37c',1,'FitFunctions.hh']]],
+  ['mwpcfinder_12',['MwpcFinder',['../classISSEventBuilder.html#a666a793f30d2556ca2a17552d4f75719',1,'ISSEventBuilder']]]
 ];
